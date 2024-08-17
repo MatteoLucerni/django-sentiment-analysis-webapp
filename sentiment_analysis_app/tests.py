af2b71afe7bce105884e24a3416c7a1f9109efa8
MatteoLucerni <lucernimatteo.off@gmail.com>
2024-08-17 18:43:58 +0200

    feat: created test case for Analysis history in test.py

--- sentiment_analysis_app/tests.py
+++ sentiment_analysis_app/tests.py
@@ -21,3 +21,23 @@ class AnalysisModelTest(TestCase):
         self.assertEqual(self.analysis.polarity, 0.9)
         self.assertEqual(self.analysis.subjectivity, 0.6)
         self.assertEqual(self.analysis.user.username, "testuser")
+
+
+class AnalysisHistoryTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword123"
+        )
+        self.client.login(username="testuser", password="testpassword123")
+        self.history_url = reverse("analysis_history")
+
+    def test_analysis_history_access(self):
+        response = self.client.get(self.history_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_analysis_history_redirect_if_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(self.history_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{reverse('login')}?next={self.history_url}")
