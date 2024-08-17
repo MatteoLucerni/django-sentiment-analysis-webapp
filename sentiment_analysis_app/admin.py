fa2261f28867f4cbf323e4674ddded417c7c0cd2
MatteoLucerni <lucernimatteo.off@gmail.com>
2024-08-17 16:04:59 +0200

    feat: declared admin Analysis entity in admin.py in sentAnaly app

--- sentiment_analysis_app/admin.py
+++ sentiment_analysis_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Analysis
 
-# Register your models here.
+
+@admin.register(Analysis)
+class AnalysisAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "input_text",
+        "polarity",
+        "subjectivity",
+        "analysis_date",
+    )
+    list_filter = ("user", "analysis_date")
+    search_fields = ("input_text",)
