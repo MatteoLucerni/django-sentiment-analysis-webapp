f94e3169ba413753bb13f76c5cff7c436a5fdfab
MatteoLucerni <lucernimatteo.off@gmail.com>
2024-08-17 15:08:42 +0200

    feat: creded view func for receiving input user text in core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_view(request):
+    if request.method == "POST":
+        user_input = request.POST.get("user_input")
+
+        return HttpResponse(f"Received text: {user_input}")
+
+    return render(request, "core/home.html")
