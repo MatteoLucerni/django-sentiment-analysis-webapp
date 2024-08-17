3762ee89aa59fb25510700133a19f3a30b87fb0d
MatteoLucerni <lucernimatteo.off@gmail.com>
2024-08-17 17:42:27 +0200

    feat: created view function for user login in user_management app

--- user_management/views.py
+++ user_management/views.py
@@ -14,3 +14,15 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, "user_management/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "user_management/login.html", {"form": form})
