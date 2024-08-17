9c0685b432ca0c4b1676b909476153e3d194d9e7
MatteoLucerni <lucernimatteo.off@gmail.com>
2024-08-17 18:42:39 +0200

    feat: created test case form Analysis model in test.py

--- sentiment_analysis_app/tests.py
+++ sentiment_analysis_app/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Analysis
 
-# Create your tests here.
+
+class AnalysisModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword123"
+        )
+        self.analysis = Analysis.objects.create(
+            user=self.user,
+            input_text="I love this product!",
+            polarity=0.9,
+            subjectivity=0.6,
+        )
+
+    def test_analysis_creation(self):
+        self.assertEqual(self.analysis.input_text, "I love this product!")
+        self.assertEqual(self.analysis.polarity, 0.9)
+        self.assertEqual(self.analysis.subjectivity, 0.6)
+        self.assertEqual(self.analysis.user.username, "testuser")
